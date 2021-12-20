280571e6b80c9b8cc6adb4d2ea3cad8e4b50d4a1
jeeni <86237336+jeeniwhitehatjr@users.noreply.github.com>
2021-12-20 15:50:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){ document.getElementById("result").innerHTML='<img id="captured_image"src="'+data_uri+'"/>';
+ }) ;
+}
+console.log("ml5version ",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Lq4dBfNcy/model.json",modelLoaded);
+function modelLoaded()
+{
+    console.log("model loaded");
+    
+}
+function check()
+{
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotresult);
+}
+function gotresult(error,results)
+{
+  if(error){
+      console.error(error);
+
+  } 
+  else{
+      console.log(results);
+      document.getElementById("result_object").innerHTML=results[0].label;
+      document.getElementById("Accuracy_object").innerHTML=results[0].confidence.toFixed(3);
+      
+  } 
+}
